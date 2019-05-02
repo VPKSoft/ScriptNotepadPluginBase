a909f0ce8eeaa43e2227efc5965d8238b1f2f9b3
VPKSoft <vpksoft@vpksoft.net>
2019-05-02 19:39:09 +0300

    Fixed the RequestScintillaDocumentEventArgs class Documents property being null by default. Updated .nuspec.

--- EventArgClasses/RequestScintillaDocumentEventArgs.cs
+++ EventArgClasses/RequestScintillaDocumentEventArgs.cs
@@ -47,14 +47,17 @@ namespace ScriptNotepadPluginBase.EventArgClasses
         /// A list of documents received from the <see cref="OnRequestActiveDocument"/> and for the <see cref="OnRequestAllDocuments"/> events.
         /// </summary>
         public List<
-            (           
+            (
             Encoding Encoding, // the document's encoding..
             Scintilla Scintilla, // the Scintilla containing the document..
             string FileNameFull, // the full file name of the document..
             DateTime FileSysModified, // the date and time the document was modified in the file system..
-            DateTime DBModified,  // the date and time the document was modified within the ScriptNotepad's database..
-            bool IsActiveDocument // A flag indicating whether the document is the active document within the ScriptNotepad..
-            )> Documents;
+            DateTime DBModified, // the date and time the document was modified within the ScriptNotepad's database..
+            bool
+            IsActiveDocument // A flag indicating whether the document is the active document within the ScriptNotepad..
+            )> Documents = // fixed null reference..
+            new List<(Encoding Encoding, Scintilla Scintilla, string FileNameFull, DateTime FileSysModified, DateTime
+                DBModified, bool IsActiveDocument)>();
     }
 
     /// <summary>
